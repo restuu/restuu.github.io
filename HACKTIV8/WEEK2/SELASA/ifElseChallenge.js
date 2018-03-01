37c98010496868517aa721f1f3c08f80913e25e1
restu <restu_u@outlook.com>
2018-03-01 16:00:10 +0700

    edit else if challenge

--- HACKTIV8/WEEK2/SELASA/ifElseChallenge.js
+++ HACKTIV8/WEEK2/SELASA/ifElseChallenge.js
@@ -1,14 +1,19 @@
 var nama = 'Kaizaki';
 var peran = 'Demon King';
 
-if (nama === 'Kaizaki' && peran === ''){
-  console.log ('Halo ' + nama + ', pilih peranmu untuk memulai game!');
-} else if (nama === 'Kaizaki' && peran === 'Demon King') {
+if (nama === ''){
+  console.log('Nama harus diisi');
+} else {
+  if (peran === 'Demon King') {
     console.log('Selamat datang di Dunia Proxytia, ' + nama);
     console.log('Halo ' + peran + ' ' + nama + ', kamu dapat memerintah bawahanmu!');
-} else if (nama === 'Hishiro' && peran === 'Healer') {
+  } else if (peran === 'Healer') {
     console.log('Selamat datang di Dunia Proxytia, ' + nama);
     console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menyembuhkan temanmu!');
-} else {
-    console.log('Nama harus diisi');
+  } else if (peran === 'Archer') {
+    console.log('Selamat datang di Dunia Proxytia, ' + nama);
+    console.log('Halo ' + peran + ' ' + nama + ', kamu dapat menggunakan senjata panah!');
+  } else {
+    console.log('Halo ' + nama + ', pilih peranmu untuk memulai game');
+  }
 }
